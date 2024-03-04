47bb44f25e8a5a7ea0804a41c18fd9886b71bc72
dyldev <dylanharahap23@gmail.com>
2024-03-04 14:48:11 +0700

    initial commit

---
+++ src/app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+// ga ada di localhost:3000/transformations
+// localhost:3000/transformations / [id]
